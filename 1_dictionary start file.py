79390352e58cd78f29106786f0bfdb7e79c98fa0
Lauren <lauren_pyka1@baylor.edu>
2024-02-05 14:13:53 -0600

    worked through section 5

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -3,29 +3,44 @@ import random
 phonebook = {'Chris':'555−1111',
              'Katie':'555−2222',
              'Joanne':'555−3333'}
-
-
+# name of dict is phonebook and there are 3 key values
+'''
 
 print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+print(phonebook)
+print(len(phonebook))
 
+mydictionary = {} #to create an empty dictionary
 
+mydictionary = dict(m=8, n=9) # m & n are the keys and 8,9 are the values 
+# dict is a function, so you have to use regular parentheses
+# keys will always be strings
 
+print(mydictionary) 
 
 print()
 print('*****  end section 1 ********')
 print()
 
 
-'''
 
 
 print()
 print('*****  start section 2 - search dictionary ********')
 print()
 
+name = 'Chris'
+
+if name in phonebook:
+    print(f"Name: {name} Phone Number: {phonebook[name]}") #don't need quotes around name bc its a variable
+else:
+    print(f"{name} is not in the phonebook")
+
+#phone = phonebook['Chris'] #dictionaries are case sensitive, if key isn't in dictionary you get 'key error'
+#print(phone)
 
 
 
@@ -46,31 +61,35 @@ print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
+print(phonebook)
 
+phonebook["Joe"] = '555-0123' #doesn't exist in dictionary, so it is added to it
+phonebook["Chris"] = '555-4444'
 
+#can't update key or change it but you can add to it. 
 
+print(phonebook)
 
 print()
 print('*****  end section 3 ********')
 print()
 
 
-
-
-
-
 print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
+print(phonebook)
 
+del phonebook["Chris"] #chris is no longer in the dictionary. 
 
+print(phonebook)
 
 print()
 print('*****  end section 4 ********')
 print()
 
-
+'''
 
 
 
@@ -79,8 +98,14 @@ print()
 print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
+#defualt for dicitionary to iterate through will always be the keys
 
+for key in phonebook:
+    print(f'The key is: {key} and the value is {phonebook[key]}')
 
+for value in phonebook.values():
+    print(value)
+#dictionaries have a method called .values that allows you to iterate through all the values of the dictionary. 
 
 
 
@@ -88,7 +113,7 @@ print()
 print('*****  end section 5 ********')
 print()
 
-
+'''
 
 
 
